8b18b69ea0272cec9d5b609600290dc73dfc42cc
areoks <aoksana@yandex.ru>
2025-12-27 22:17:48 +0300

    fix: реализовывает сценарии загрузки изображения и его редактирования

--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
 import { getAllPhotoUsers } from './create-photos/create-data.js';
 import { renderPictures } from './create-photos/thumbnails.js';
 import { initUploadForm } from './form-validation/upload-photo-form.js';
+import { scaleListener } from './image-editing/scale.js';
+import { effectCheckedListener } from './image-editing/slider.js';
 
 const photos = getAllPhotoUsers();
 renderPictures(photos);
 
+scaleListener();
+effectCheckedListener();
 initUploadForm();
