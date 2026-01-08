73778ea5559af4d03fc1c1229945b27c3e11bad9
areoks <aoksana@yandex.ru>
2026-01-08 10:03:53 +0300

    fix: подключает модуль server.js в проект

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ import { renderPictures } from './create-photos/thumbnails.js';
 import { initUploadForm } from './form-validation/upload-photo-form.js';
 import { scaleListener, resetScale } from './image-editing/scale.js';
 import { effectCheckedListener, resetEffects } from './image-editing/slider.js';
+import { serverFetch } from './fetch/server.js';
 
 const photos = getAllPhotoUsers();
 renderPictures(photos);
@@ -11,3 +12,4 @@ resetScale();
 effectCheckedListener();
 resetEffects();
 initUploadForm();
+serverFetch();
