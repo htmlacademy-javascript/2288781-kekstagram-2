cfa8705c2f17b84a82f769a42a29a4453bc8e661
Keks <keks@htmlacademy.ru>
2026-01-15 20:56:04 +0300

    :heavy_check_mark: Сборка #27

---
+++ 27/js/form-validation/form-data.js
@@ -0,0 +1,54 @@
+const uploadForm = document.querySelector('.img-upload__form'); // Форма загрузки нового изображения
+
+const uploadFileControl = uploadForm.querySelector('.img-upload__input'); // Выбор файла с изображением для загрузки
+
+const photoEditorForm = uploadForm.querySelector('.img-upload__overlay'); // Применение одного из заранее заготовленных эффектов
+const photoEditorScale = photoEditorForm.querySelector('.img-upload__scale'); // Изменение масштаба изображения
+const photoEditorResetButton = photoEditorForm.querySelector('.img-upload__cancel'); // Выбор глубины эффекта с помощью ползунка
+
+const hashtagsInput = uploadForm.querySelector('.text__hashtags'); // Добавление хэштегов
+const descriptionInput = uploadForm.querySelector('.text__description'); // Добавление текстового комментария
+
+const submitButton = document.querySelector('.img-upload__submit');
+
+const pageBody = document.querySelector('body'); // Тег body страницы
+
+
+// Настройки и сообщения для валидации формы загрузки изображения
+// Настройки и сообщения для валидации формы загрузки изображения
+const DATA_FORM_SET = {
+  HASHTAG: /^#[a-zа-яё0-9]{1,19}$/i,
+  HASHTAGS_LIMIT: 5,
+  MAX_HASHTAG_LENGTH: 20,
+  MAX_DESCRIPTION_LENGTH: 140,
+};
+
+// Сообщения об ошибках для формы загрузки изображения
+// Сообщения об ошибках для формы загрузки изображения
+const ERROR_MESSAGE = {
+  ERROR_MAX_LENGTH_DESCRIPTION: `Максимальная длина комментария не более ${DATA_FORM_SET.MAX_DESCRIPTION_LENGTH} символов`,
+  ERROR__HASHTAG: 'Хэштег должен начинаться с символа # и содержать только буквы и цифры без пробелов и спецсимволов',
+  ERROR_MAX_LENGTH_HASHTAG: `Максимальная длина одного хэштега ${DATA_FORM_SET.MAX_HASHTAG_LENGTH} символов, включая решётку`,
+  ERROR_MAX_HASHTAGS: 'Превышено количество хэштегов, их должно быть не более 5',
+  ERROR_REPEAT: 'Хэштеги повторяются'
+};
+
+export {
+  uploadForm,
+
+  uploadFileControl,
+
+  photoEditorForm,
+  photoEditorScale,
+  photoEditorResetButton,
+
+  descriptionInput,
+  hashtagsInput,
+
+  submitButton,
+
+  pageBody,
+
+  DATA_FORM_SET,
+  ERROR_MESSAGE
+};
---
+++ 27/js/rendering-big-photos/constanta-big-photos.js
@@ -0,0 +1,35 @@
+// МОДУЛЬ С КОНСТАНТАМИ, СВЯЗАННЫМИ С ДАННЫМИ ПОЛНОРАЗМЕРНЫХ ИЗОБРАЖЕНИЙ
+
+const pageBody = document.querySelector('body'); // Тег body страницы
+
+const bigPicture = document.querySelector('.big-picture');
+const bigPictureButtonClose = bigPicture.querySelector('.big-picture__cancel');
+
+const bigImage = document.querySelector('.big-picture__img img');
+const bigImageLikes = document.querySelector('.likes-count');
+
+const commentsList = document.querySelector('.social__comments');
+const template = document.querySelector('.social__comment');
+const bigImageCaption = document.querySelector('.social__caption');
+const bigImageShownCommentsCount = document.querySelector('.social__comment-shown-count');
+const bigImageTotalCommentsCount = document.querySelector('.social__comment-total-count');
+
+
+const bigImageLoader = document.querySelector('.comments-loader');
+
+const COMMENTS_LIMIT = 5;
+
+export {
+  pageBody,
+  bigPicture,
+  bigPictureButtonClose,
+  bigImage,
+  bigImageLikes,
+  commentsList,
+  template,
+  bigImageCaption,
+  bigImageShownCommentsCount,
+  bigImageTotalCommentsCount,
+  bigImageLoader,
+  COMMENTS_LIMIT
+};
